3a21f4d8c4c1c72db220039314d5500a891d3d28
Gma_Gama <nagahama.takuya@outlook.jp>
2022-05-16 22:38:29 +0900

    feat(cypress/lighthouse) add clighthouse command into cypress

--- cypress/integration/sample.spec.ts
+++ cypress/integration/sample.spec.ts
@@ -1,6 +1,16 @@
 describe("", () => {
+    beforeEach(() => {
+        cy.visit("/")
+    })
+
     it("/", () => {
-        cy.visit("")
         cy.location().url().should("include", "/")
+        cy.lighthouse("http://localhost:3000").then(res => {
+            cy.wrap(res).its("accessibility").should("gt", 0.9)
+            cy.wrap(res).its("best-practices").should("gt", 0.9)
+            cy.wrap(res).its("performance").should("gt", 0.9)
+            cy.wrap(res).its("pwa").should("gt", 0.9)
+            cy.wrap(res).its("seo").should("gt",0.9)
+        });
     })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,10 +1,24 @@
 /// <reference types="cypress" />
+
 declare namespace Cypress {
-  interface Chainable<Subject> {
-    clickLink(label: string): void
+  interface Chainable<Subject = any> {
+    clickLink(label: string): Chainable<Subject>
+    lighthouse(url: string): Promise<any>
   }
 }
 
 Cypress.Commands.add('clickLink', (label: string | number | RegExp) => {
   cy.get('a').contains(label).click()
+})
+
+Cypress.Commands.add("lighthouse", (url: string) => {
+  cy.exec(`lighthouse ${url} --chrome-flags="--headless" --output="json" `).then(({ stdout }) => {
+    const { categories } = JSON.parse(stdout);
+    return Object.keys(categories).reduce((acc, key) => {
+      return {
+        ...acc,
+        [key]: categories[key].score
+      }
+    }, {});
+  });
 })
